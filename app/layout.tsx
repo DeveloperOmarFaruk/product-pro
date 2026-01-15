02056772b24fe9bbe34dfb66dcacc8d8e9a9cf99
Omar Faruk <omarfarukcourse@gmail.com>
2026-01-15 13:15:43 +0600

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,8 @@
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { BrowserRouter } from "react-router-dom";
 
 export default function RootLayout({
   children,
@@ -17,10 +15,8 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Provider store={store}>
-          <BrowserRouter>
-            {children}
-            <ToastContainer />
-          </BrowserRouter>
+          {children}
+          <ToastContainer />
         </Provider>
       </body>
     </html>
